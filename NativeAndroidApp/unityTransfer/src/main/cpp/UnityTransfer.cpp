9616c720c3afdcc768a674297b4366b15b5435b8
zhanghaoxian <zhxmdefj@outlook.com>
2020-07-16 14:58:13 +0800

    UnityTransfer

---
+++ NativeAndroidApp/unityTransfer/src/main/cpp/UnityTransfer.cpp
@@ -0,0 +1,144 @@
+//
+// Created by iFinVer on 2016/12/13.
+//
+
+#include "UnityTransfer.h"
+#include "includes/faceresult.h"
+#include "log.h"
+#include <cmath>
+
+#define LOG_TAG "UnityTransfer"
+
+UnityTransfer::UnityTransfer() {
+    this->mUnityMsg = new UnityMsg();
+    this->mFaceMsg = new FaceMsg();
+}
+
+void UnityTransfer::setTransferByUnity(UnityTransfer::Transfer transfer) {
+    this->mTransfer = transfer;
+}
+
+void UnityTransfer::setFaceTransferByUnity(FaceTransfer transfer) {
+    this->mFaceTransfer = transfer;
+}
+
+void UnityTransfer::setMonalisaTransferByUnity(MonalisaTransfer transfer) {
+    this->mMonalisaTransfer = transfer;
+}
+
+#ifdef USE_UNITY_NATIVE_SEND_MESSAGE
+void UnityTransfer::setVideoRecordActionTransferByUnity(VideoRecordActionTransfer transfer) {
+    this->mVideoRecordActionTransfer = transfer;
+}
+
+void UnityTransfer::setEnableBlurTransferByUnity(EnableBlurTransfer transfer) {
+    this->mEnableBlurTransfer = transfer;
+}
+
+void UnityTransfer::setPauseAssetAudioTransferByUnity(PauseAssetAudioTransfer transfer) {
+    this->mPauseAssetAudioTransfer = transfer;
+}
+
+void UnityTransfer::setInitAssetsLoaderTransferByUnity(InitAssetsLoaderTransfer transfer) {
+    this->mInitAssetsLoaderTransfer = transfer;
+}
+
+void UnityTransfer::setSelectAssetTransferByUnity(SelectAssetTransfer transfer) {
+    this->mSelectAssetTransfer = transfer;
+}
+
+void UnityTransfer::setCleanUpAssetsCacheTransferByUnity(CleanUpAssetsCacheTransfer transfer) {
+    this->mCleanUpAssetsCacheTransfer = transfer;
+}
+#endif
+
+int dis(const MPOINT *p1, const MPOINT *p2) {
+    return (int) std::sqrt((p2->x - p1->x) * (p2->x - p1->x) + (p2->y - p1->y) * (p2->y - p1->y));
+}
+
+void UnityTransfer::transformToUnity(jbyte *yuvData, int width, int height, int degree, jboolean mirror, jlong facePtr) {
+    //宽高改变时重新初始化
+    if (mUnityMsg->uvPtr == nullptr || mUnityMsg->width != width || mUnityMsg->height != height) {
+        mUnityMsg->width = width;
+        mUnityMsg->height = height;
+        if (mUnityMsg->uvPtr != nullptr) {
+            delete[] mUnityMsg->uvPtr;
+        }
+        mUnityMsg->uvPtr = new unsigned char[width * height * 3 / 4];
+    }
+    //处理uv通道
+    int yLen = width * height;
+    int yuvLen = yLen * 3 / 2;
+    int dstPtr = 0;
+    for (int i = yLen; i < yuvLen; i += 2) {
+        mUnityMsg->uvPtr[dstPtr++] = (unsigned char) (yuvData[i] & 0xFF);
+        mUnityMsg->uvPtr[dstPtr++] = (unsigned char) (yuvData[i + 1] & 0xFF);
+        mUnityMsg->uvPtr[dstPtr++] = 0;
+    }
+    //y通道赋值
+    mUnityMsg->yPtr = yuvData;
+    //旋转角度
+    mUnityMsg->degree = degree;
+    //翻转控制
+    mUnityMsg->mirror = mirror ? 1 : 0;
+
+    //处理人脸数据
+    FaceDetectResult *faceData = nullptr;
+    try {
+        if (facePtr != 0) {
+            faceData = (FaceDetectResult *) facePtr;
+        }
+    } catch (...) {
+    }
+    if (faceData != nullptr) {
+        mFaceMsg->degree = degree;
+        mFaceMsg->width = width;
+        mFaceMsg->height = height;
+        mFaceMsg->mirror = mirror ? 1 : 0;
+        mFaceMsg->faceCount = faceData->nFaceCountInOut;
+        mFaceMsg->faceOutlinePoint = faceData->pFaceOutlinePointOut;
+        mFaceMsg->faceDetectRect = faceData->rcFaceRectOut;
+        mFaceMsg->faceOrientation = faceData->faceOrientOut;
+//        if(faceData->nFaceCountInOut > 0){
+////            MFloat fRoll = faceData->faceOrientOut[0];
+////            MFloat fRaw = faceData->faceOrientOut[1];
+////            MFloat fPitch = faceData->faceOrientOut[2];
+//            MRECT rect = faceData->rcFaceRectOut[0];
+////            LOGE("fRoll=%.3f,fRaw=%.3f,fPitch=%.3f,degree=%d,rectWidth=%d,rectHeight=%d",fRoll,fRaw,fPitch,degree,rect.right - rect.left,rect.bottom - rect.top);
+//            MPOINT &p97 = faceData->pFaceOutlinePointOut[97];
+//            MPOINT &p99 = faceData->pFaceOutlinePointOut[99];
+//            MPOINT &p95 = faceData->pFaceOutlinePointOut[95];
+//            MPOINT &p96 = faceData->pFaceOutlinePointOut[96];
+//            LOGE("rectWidth=%d,rectHeight=%d,dis97-99=%d,dis95-96=%d",rect.right - rect.left,rect.bottom - rect.top,dis(&p97,&p99),dis(&p95,&p96));
+//        }
+
+    } else {
+        mFaceMsg->faceCount = 0;
+    }
+
+    //传送给Unity
+    transform();
+}
+
+void UnityTransfer::transformMonalisa(jlong monalisaMsgPtr) {
+    if (mMonalisaTransfer != nullptr && monalisaMsgPtr != 0) {
+        MonalisaMsg *msg = (MonalisaMsg *) monalisaMsgPtr;
+//        if (mMonalisaMsg == nullptr) {
+//            mMonalisaMsg = new MonalisaMsg();
+//            mMonalisaMsg->width = msg->cols;
+//            mMonalisaMsg->height = msg->rows;
+//            mMonalisaMsg->texSize = mMonalisaMsg->width*mMonalisaMsg->height*3;
+//        }
+//        cv::cvtColor(*msg, temp, CV_BGR2RGB);
+//        //旋转180
+//        if(matrix == nullptr){
+//            matrix = new float[6];
+//            CvMat M = cvMat( 2, 3, CV_32F, matrix);
+//            CvPoint2D32f center = CvPoint2D32f(temp.cols/2,temp.rows/2);
+//            cv2DRotationMatrix( center, 180,1, &M);
+//            cvWarpAffine(img,img_rotate, &M,CV_INTER_LINEAR+CV_WARP_FILL_OUTLIERS,cvScalarAll(0) );
+//        }
+//        mMonalisaMsg->texData = temp.data;
+        mMonalisaTransfer(msg);
+    }
+}
---
+++ NativeAndroidApp/unityTransfer/src/main/cpp/main.cpp
@@ -0,0 +1,169 @@
+//
+// Created by iFinVer on 2016/12/13.
+//
+
+#include "main.h"
+#include "UnityTransfer.h"
+
+
+UnityTransfer *mConnector = nullptr;
+
+/**
+ * will be invoked by java code
+ */
+extern "C" JNIEXPORT void JNICALL
+Java_com_zhx_unitytransfer_UnityTransfer_onVideoBuffer(
+        JNIEnv *env, jclass , jbyteArray data_, jint frameWidth, jint frameHeight, jint degree,jboolean mirror,jlong facePtr) {
+    jbyte *data = env->GetByteArrayElements(data_, 0);
+    if(mConnector != nullptr){
+        mConnector->transformToUnity(data,frameWidth,frameHeight,degree,mirror,facePtr);
+    }
+    env->ReleaseByteArrayElements(data_, data, JNI_ABORT);
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_zhx_unitytransfer_UnityTransfer_initAssetsLoader(JNIEnv *env, jclass type, jstring json_) {
+#ifdef USE_UNITY_NATIVE_SEND_MESSAGE
+    char *json = (char *) env->GetStringUTFChars(json_, 0);
+    if(mConnector != nullptr){
+        mConnector->transferInitAssetsLoader(json);
+    }
+    env->ReleaseStringUTFChars(json_, json);
+#endif
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_zhx_unitytransfer_UnityTransfer_loadAsset(JNIEnv *env, jclass type, jint assetId) {
+#ifdef USE_UNITY_NATIVE_SEND_MESSAGE
+    if(mConnector != nullptr){
+        mConnector->transferSelectAsset(assetId);
+    }
+#endif
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_zhx_unitytransfer_UnityTransfer_setRecordAction(JNIEnv *env, jclass type, jint recordAction) {
+#ifdef USE_UNITY_NATIVE_SEND_MESSAGE
+    if(mConnector != nullptr){
+        mConnector->transferVideoRecordingAction((UnityTransfer::RecordingAction) recordAction);
+    }
+#endif
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_zhx_unitytransfer_UnityTransfer_enableBlur(JNIEnv *env, jclass type,
+                                                        jboolean isEnable) {
+#ifdef USE_UNITY_NATIVE_SEND_MESSAGE
+    if(mConnector != nullptr){
+        mConnector->transferEnableBlur(isEnable);
+    }
+#endif
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_zhx_unitytransfer_UnityTransfer_pauseAssetAudio(JNIEnv *env, jclass type,
+                                                             jboolean isPause) {
+#ifdef USE_UNITY_NATIVE_SEND_MESSAGE
+    if(mConnector != nullptr){
+        mConnector->transferPauseAssetAudio(isPause);
+    }
+#endif
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_zhx_unitytransfer_UnityTransfer_cleanUpAssetsCache(JNIEnv *env, jclass type) {
+#ifdef USE_UNITY_NATIVE_SEND_MESSAGE
+    if(mConnector != nullptr){
+        mConnector->transferCleanUpAssetsCache();
+    }
+#endif
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_zhx_unitytransfer_UnityTransfer_onMonalisaData(JNIEnv *env, jclass type, jlong msgPtr) {
+    if(mConnector != nullptr){
+        mConnector->transformMonalisa(msgPtr);
+    }
+}
+
+
+/**
+ * will be invoked by unity's scripts
+ */
+//void setTransferByUnity(UnityTransfer::Transfer transfer) {
+//    if (mConnector == nullptr) {
+//        mConnector = new UnityTransfer();
+//    }
+//    mConnector->setTransferByUnity(transfer);
+//}
+//
+//void setFaceTransferByUnity(UnityTransfer::FaceTransfer transfer){
+//    if (mConnector == nullptr) {
+//        mConnector = new UnityTransfer();
+//    }
+//    mConnector->setFaceTransferByUnity(transfer);
+//}
+
+void UnitySetCameraRenderFuc(UnityTransfer::Transfer transfer) {
+    if (mConnector == nullptr) {
+        mConnector = new UnityTransfer();
+    }
+    mConnector->setTransferByUnity(transfer);
+}
+
+void UnitySetOnFaceDetectedFuc(UnityTransfer::FaceTransfer transfer){
+    if (mConnector == nullptr) {
+        mConnector = new UnityTransfer();
+    }
+    mConnector->setFaceTransferByUnity(transfer);
+}
+
+void setMonalisaCallbackByUnity(UnityTransfer::MonalisaTransfer transfer){
+    if (mConnector == nullptr) {
+        mConnector = new UnityTransfer();
+    }
+    mConnector->setMonalisaTransferByUnity(transfer);
+}
+#ifdef USE_UNITY_NATIVE_SEND_MESSAGE
+void UnitySetVideoRecordingFuc(UnityTransfer::VideoRecordActionTransfer transfer) {
+    if (mConnector == nullptr) {
+        mConnector = new UnityTransfer();
+    }
+    mConnector->setVideoRecordActionTransferByUnity(transfer);
+};
+
+void UnitySetEnableBlurFuc(UnityTransfer::EnableBlurTransfer transfer) {
+    if (mConnector == nullptr) {
+        mConnector = new UnityTransfer();
+    }
+    mConnector->setEnableBlurTransferByUnity(transfer);
+};
+
+void UnitySetPauseAssetAudioFuc(UnityTransfer::PauseAssetAudioTransfer transfer) {
+    if (mConnector == nullptr) {
+        mConnector = new UnityTransfer();
+    }
+    mConnector->setPauseAssetAudioTransferByUnity(transfer);
+};
+
+void UnitySetSelectAssetObjectFuc(UnityTransfer::SelectAssetTransfer transfer) {
+    if (mConnector == nullptr) {
+        mConnector = new UnityTransfer();
+    }
+    mConnector->setSelectAssetTransferByUnity(transfer);
+};
+
+void UnitySetCleanUpAssetsCacheFuc(UnityTransfer::CleanUpAssetsCacheTransfer transfer) {
+    if (mConnector == nullptr) {
+        mConnector = new UnityTransfer();
+    }
+    mConnector->setCleanUpAssetsCacheTransferByUnity(transfer);
+};
+
+void UnitySetInitAssetsLoaderFuc(UnityTransfer::InitAssetsLoaderTransfer transfer) {
+    if (mConnector == nullptr) {
+        mConnector = new UnityTransfer();
+    }
+    mConnector->setInitAssetsLoaderTransferByUnity(transfer);
+};
+#endif
